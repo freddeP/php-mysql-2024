89ab4f56adef110963eadff44dd840a3e56e0929
mrPefr <persson.fredric@gmail.com>
2024-10-14 08:44:51 +0200

    file upload started

--- index.php
+++ index.php
@@ -16,19 +16,59 @@ App::get("/upload", function(){
 
 App::post("/upload", function(){
 
-   debug($_POST);
-   debug($_FILES);
-
-   $name = "file_".uniqid();
+   $name = "file_".uniqid(true);
    
-   $ext = explode(".",$_FILES['img']['name']);
-   $length = count($ext) - 1;
-   $ext = $ext[$length];
+   $ext = Upload::getExt($_FILES['img']['name']);
  
+   if(!Upload::checkExt($ext)){
+      echo "wrong extention";
+      return;
+   }
 
+   if(!Upload::checkSize($_FILES['img'])){
+      echo "Too large file max size 3Mb";
+      return;
+   }
    $name = $name . "." . $ext;
-
-   echo $name;
+   move_uploaded_file($_FILES['img']['tmp_name'], "uploads/$name");
 
 
 });
+
+
+class Upload{
+
+
+   public static function getExt($fileName){
+
+      $ext = explode(".",$fileName);
+      $length = count($ext) - 1;
+      return $ext[$length];
+
+   }
+   public static function checkExt($ext){
+
+      $extentions = [
+         "jpg"=>true,
+         "JPG"=>true,
+         "png"=>true,
+         "PNG"=>true,
+         "gif"=>true
+      ];
+
+      if(empty($extentions[$ext])) return false;
+
+      return true;
+
+   }
+
+   public static function checkSize($file){
+
+      if($file['size']>3000000) return false;
+
+      return true;
+
+   }
+
+
+}
